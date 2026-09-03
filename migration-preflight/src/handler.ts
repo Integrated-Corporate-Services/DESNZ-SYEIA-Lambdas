63ca56b243a5962b26060cd34afbb1c2156c3876
Tri-desnz <tribhuwan.dhapola@ics.gov.uk>
2026-09-03 08:28:01 +0100

    SYEIA-2522 Add migration preflight Lambda provisioning stub

---
+++ migration-preflight/src/handler.ts
@@ -0,0 +1,7 @@
+import { Handler } from 'aws-lambda';
+
+export const handler: Handler = async (_event, context) => ({
+  outcome: 'SKIPPED',
+  correlationId: context.awsRequestId,
+  message: 'Migration preflight is provisioned but not enabled',
+});
